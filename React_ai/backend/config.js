a96439fcd321c9d786df0ffa4e8694c11fcd1d53
Kyaw Min Oo <122626962+KyawMinOo123-cyber@users.noreply.github.com>
2023-10-06 22:27:53 +0630

    Update config.js
    removed database password

--- React_ai/backend/config.js
+++ React_ai/backend/config.js
@@ -1,2 +1,2 @@
-export const mongoDBURL ='mongodb+srv://KyawMinOo:dImdKAVDKcNLykfI@cluster0.xmzmln9.mongodb.net/book-collection?retryWrites=true&w=majority'
+export const mongoDBURL ='mongodb+srv://@cluster0.xmzmln9.mongodb.net/book-collection?retryWrites=true&w=majority'
 export const PORT = 5555;
